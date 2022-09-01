635f622a3a0c999a2ca618e31263789549a67775
Symon <nuberman88@gmail.com>
2022-09-01 17:57:39 +0400

    page-header переименован в header

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,16 @@
-menu = document.querySelector('.page-header__nav');
-menuToggler = document.querySelector('.page-header__menu-toggler');
+menu = document.querySelector('.header__nav');
+menuToggler = document.querySelector('.header__menu-toggler');
 
-menu.classList.add('page-header__nav--hidden');
+menu.classList.add('header__nav--hidden');
 
 menuToggler.addEventListener('click', () => {
-  if (menu.classList.contains('page-header__nav--hidden')) {
-    menu.classList.remove('page-header__nav--hidden');
-    menuToggler.classList.remove('page-header__menu-toggler--to-open');
-    menuToggler.classList.add('page-header__menu-toggler--to-close');
+  if (menu.classList.contains('header__nav--hidden')) {
+    menu.classList.remove('header__nav--hidden');
+    menuToggler.classList.remove('header__menu-toggler--to-open');
+    menuToggler.classList.add('header__menu-toggler--to-close');
   } else {
-    menu.classList.add('page-header__nav--hidden');
-    menuToggler.classList.add('page-header__menu-toggler--to-open');
-    menuToggler.classList.remove('page-header__menu-toggler--to-close');
+    menu.classList.add('header__nav--hidden');
+    menuToggler.classList.add('header__menu-toggler--to-open');
+    menuToggler.classList.remove('header__menu-toggler--to-close');
   }
 });
